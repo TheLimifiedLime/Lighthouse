c5315bae9f0efc35fef65986fb2f1c7320b49c95
TheLimifiedLime <thelimifiedlime@protonmail.com>
2021-01-07 02:18:23 +0000

    Magic

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const rawToken = window.location.href.match(/access_token=[+A-z0-9]{1,30}/);
 if (!rawToken) {
-  window.location.href = `https://discord.com/oauth2/authorize?prompt=none&response_type=token&client_id=796490540619137024&state=15773059ghq9183habn&scope=identify%20guilds`;
+  window.location.href = `https://discord.com/api/oauth2/authorize?client_id=796490540619137024&redirect_uri=https%3A%2F%2Fcounter.issai.club&response_type=code&scope=guilds%20identify`;
 }
 const token = rawToken[0].replace("access_token=", "");
-//alert(token)
+window.history.pushState({}, '', ' ');
 
 axios
   .get("https://discord.com/api/users/@me", {
@@ -34,11 +34,11 @@ axios
     },
   })
   .then(function (response) {
-    // handle success
-    //document.getElementById('counter-container').innerText = `You are in guilds`
     document.getElementById(
       "counter-container"
-    ).innerHTML = `You are in <span id="counter" class="text-center text-green-500 text-3xl font-mono">${response.data.length}</span>`;
+    ).innerHTML = `You are in <span id="counter" class="text-center text-green-500 text-3xl font-mono">${response.data.length}</span> guilds!`;
+    document.getElementById('spinner').style.display = `none`
+    document.getElementById('curtain').style.display = `block`
   })
   .catch(function (error) {
     // handle error
