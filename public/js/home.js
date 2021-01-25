ce30b67a7dd7ce85dbf2190c85279cae69b6ba9d
TheLimifiedLime <thelimifiedlime@protonmail.com>
2021-01-25 15:27:15 +0000

    Add comments to explain code

--- public/js/home.js
+++ public/js/home.js
@@ -1,13 +1,21 @@
 async function run() {
+  // Here we use regex to get the access token from the callback
   const rawToken = await window.location.href.match(
     /access_token=[+A-z0-9]{1,30}/
   );
+
+  // If there isn't a token in the url (like when opening the site for the first time) it redirects the user to the auth page
   if (!rawToken) {
     window.location.href = `https://discord.com/api/oauth2/authorize?client_id=796490540619137024&prompt=none&redirect_uri=https%3A%2F%2FLighthouse-Beta.thelimifiedlime.repl.co&response_type=token&scope=guilds%20identify`;
   }
+
+  // If there is a token it removes the first part to leave the token by itself
   const token = await rawToken[0].replace("access_token=", "");
+
+  // "Clears" the ur at the top of the users browser to prevent showing the token any longer than necessary
   //window.history.pushState({}, "", " ");
 
+  // Sends a request to Discord's API to fetch basic user info used for the picture and greeting
   await axios
     .get("https://discord.com/api/users/@me", {
       headers: {
@@ -15,18 +23,22 @@ async function run() {
       },
     })
     .then(function (response) {
+      // Edits the image element and set it to the users avatar
       document.getElementById(
         "avatar"
       ).src = `https://cdn.discordapp.com/avatars/${response.data.id}/${response.data.avatar}?size=4096`;
+      // Adds the border around the image
       document.getElementById("avatar").classList.add("border-8");
+      // Adds the greeting to the welcome element
       document.getElementById(
         "welcome"
       ).innerText = `Hey, ${response.data.username}#${response.data.discriminator}`;
     })
     .catch(function (error) {
-      console.log(error);
+      console.log(error)
     });
 
+  // Fetches the guilds from the user so we can count them
   await axios
     .get("https://discord.com/api/users/@me/guilds", {
       headers: {
@@ -34,17 +46,18 @@ async function run() {
       },
     })
     .then(function (response) {
+      // Edits the counter to display the number of guilds
       document.getElementById(
         "counter"
       ).innerHTML = `You are in <span id="counter" class="text-center text-green-500 text-5xl font-mono">${response.data.length}</span> servers!`;
-      document.getElementById("spinner").style.display = `none`;
-      document.getElementById("curtain").style.display = `block`;
     })
     .catch(function (error) {
-      console.log(error);
+      console.log(error)
     });
-  document.getElementById("spinner").style.display = `none`;
-  document.getElementById("curtain").style.display = `block`;
+        // Hides spinner
+      document.getElementById("spinner").style.display = `none`;
+      // Unhides everything elseo covered by the "curtain"
+      document.getElementById("curtain").style.display = `block`;
 }
 
 run();
