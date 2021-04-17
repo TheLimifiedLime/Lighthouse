bed8831736a4d5573d865e9b5133b5ed70cd9e93
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-04-17 01:38:17 +0000

    Enable dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: "jit",
   purge: ["./public/*.html", "./public/*.js"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: true, // or 'media' or 'class'
   theme: {
     extend: {},
   },
