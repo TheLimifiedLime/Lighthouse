0b5f4b87cbf8839f946f09cceeb242d228dc83f2
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-05-18 15:40:53 +0000

    Check localstorage for theme preference

--- public/scripts/dist/theme.js
+++ public/scripts/dist/theme.js
@@ -1,4 +1,17 @@
 "use strict";
-var toggleTheme = function () {
-    document.documentElement.classList.toggle("dark");
+var theme = function () {
+    // Try to get the user's theme preference from localstorage
+    var theme = localStorage.getItem("theme");
+    /*
+      Check if the user's theme preference is dark mode
+      or if their browser indicates that they prefer a dark color scheme and set it to dark mode.
+      Otherwise set it to light mode
+    */
+    if (theme === "dark" ||
+        window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        return document.documentElement.classList.add("dark");
+    }
+    else {
+        return document.documentElement.classList.remove("dark");
+    }
 };
--- public/scripts/src/theme.ts
+++ public/scripts/src/theme.ts
@@ -1,3 +1,17 @@
-const toggleTheme = () => {
-  document.documentElement.classList.toggle("dark");
+const theme = () => {
+  // Try to get the user's theme preference from localstorage
+  const theme = localStorage.getItem("theme");
+  /*
+    Check if the user's theme preference is dark mode
+    or if their browser indicates that they prefer a dark color scheme and set it to dark mode.
+    Otherwise set it to light mode
+  */
+  if (
+    theme === "dark" ||
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    return document.documentElement.classList.add("dark");
+  } else {
+    return document.documentElement.classList.remove("dark");
+  }
 };
