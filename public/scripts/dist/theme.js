ff4bc36f4b42312e8de8caef86df77ec86ac9321
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-05-18 15:45:25 +0000

    Minor: Fix multi-line comment formatting

--- public/scripts/dist/theme.js
+++ public/scripts/dist/theme.js
@@ -5,7 +5,7 @@ var theme = localStorage.getItem("theme");
     Check if the user's theme preference is dark mode
     or if their browser indicates that they prefer a dark color scheme and set it to dark mode.
     Otherwise set it to light mode
-  */
+*/
 if (theme === "dark" ||
     window.matchMedia("(prefers-color-scheme: dark)").matches) {
     document.documentElement.classList.add("dark");
--- public/scripts/src/theme.ts
+++ public/scripts/src/theme.ts
@@ -4,7 +4,7 @@ const theme = localStorage.getItem("theme");
     Check if the user's theme preference is dark mode
     or if their browser indicates that they prefer a dark color scheme and set it to dark mode.
     Otherwise set it to light mode
-  */
+*/
 if (
   theme === "dark" ||
   window.matchMedia("(prefers-color-scheme: dark)").matches
